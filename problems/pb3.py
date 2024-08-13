e3aa09452d39692ced930ca2afbb653d6e0c7731
NerdOctopus <37950583+NerdOctopus@users.noreply.github.com>
2024-08-13 14:26:31 -0500

    Update pb3.py

--- problems/pb3.py
+++ problems/pb3.py
@@ -1,6 +1,12 @@
 # declare and fill out function here
-
-
+def youngest_student2(dict):
+	youngest = 999
+	yname = "Ryan"
+	for x, y in dict.items():
+		if (y < youngest):
+			youngest = y
+			yname = x
+	return yname
 
 # test case
 students = {"Drake": 21, "Alice": 18, "Bob": 20, "Charlie": 19, "David": 22, "Jay": 20}
