6c4be14b4d1aa3473971323facc76a77e9447895
NerdOctopus <37950583+NerdOctopus@users.noreply.github.com>
2024-08-13 14:26:06 -0500

    Update pb2.py

--- problems/pb2.py
+++ problems/pb2.py
@@ -1,4 +1,22 @@
 # declare and fill out function here
+def max_values2(array):
+	a = -99999
+	b = -99999
+	ai = 0
+	bi = 0
+	i = 0
+	for x in array:
+		print (array[i])
+		if (x > a):
+			b = a
+			bi = ai
+			a = array[i]
+			ai = i
+		elif (x > b):
+			b = array[i]
+			bi = i
+		i = i+1
+	return [ai,bi]
 
 # test case
 print(max_values2([-5, -2, -1, -11])) # -> [1, 2]
